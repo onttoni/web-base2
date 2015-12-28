36b2f633fb9539c81de7096e538c97f1f9e0ba67
Mikko Karvonen <mikko.karvonen@ymail.com>
2015-12-28 18:55:46 +0200

    Implement signin component.

--- src/public/app/components/user/signin.ts
+++ src/public/app/components/user/signin.ts
@@ -1,15 +1,35 @@
 import {Component} from 'angular2/core';
+import {Router, RouterLink} from 'angular2/router';
+import {User, UserService} from '../../shared/services/userService';
 
 @Component({
-  directives: [],
+  directives: [RouterLink],
   pipes: [],
   providers: [],
   selector: 'signin',
   template: require('./signin.html')
 })
 export class UserSignin {
-  constructor() {
+  public email: string;
+  public password: string;
+
+  constructor(private _router: Router, private _userService: UserService) {
     console.debug('UserSignin constructor.');
+  }
+
+  public onCancel(): void {
+    this._router.navigate(['Home']);
+  }
+
+  public onSignin(): void {
+    this._userService.user$.subscribe(
+      (user: User) => {
+        if (user !== null) {
+          this._router.navigate(['Home']);
+        }
+      }
+    );
+    this._userService.signin(new User(this.email, this.password));
   }
 
 }
