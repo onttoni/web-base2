4985ea1f4e501644597d80bedcac52ce8da8a4b4
Mikko Karvonen <mikko.karvonen@ymail.com>
2015-12-28 18:52:34 +0200

    Implement signin and signout in UserService.

--- src/public/app/shared/services/userService.ts
+++ src/public/app/shared/services/userService.ts
@@ -1,7 +1,18 @@
 import {Injectable} from 'angular2/core';
-import {Http} from 'angular2/http';
+import {BaseRequestOptions, Headers, Http, URLSearchParams} from 'angular2/http';
 import {Observable} from 'rxjs/Observable';
 import _ = require('lodash');
+
+
+class SigninRequestOptions extends BaseRequestOptions {
+  public search: URLSearchParams = new URLSearchParams('login=true');
+  public headers: Headers = new Headers({'Content-Type': 'application/json'});
+}
+
+
+class SignoutRequestOptions extends BaseRequestOptions {
+  public search: URLSearchParams = new URLSearchParams('logout=true');
+}
 
 
 interface UserInterface {
@@ -15,14 +26,12 @@ interface UserInterface {
 
 
 export class User implements UserInterface {
-  public email: string;
   public name: {
     formatted: string;
   };
-  public password: string;
   public token: string;
 
-  constructor() {
+  constructor(public email: string, public password: string) {
     console.debug('User constructor.');
   }
 }
@@ -47,18 +56,22 @@ export class UserService {
     if (this._user !== null) {
       return this._user;
     }
-    this._http.get('/api/users/')
+    this._http.get('/api/users')
     .map(response => response.json())
     .subscribe(data => this._setSignedIn(data), error => this._unsetSignedIn());
     return null;
   }
 
-  public signin(): void {
-
+  public signin(user: User): void {
+    this._http.post('/api/users', JSON.stringify(user), new SigninRequestOptions())
+    .map(response => response.json())
+    .subscribe(data => this._setSignedIn(data), error => this._unsetSignedIn());
   }
 
   public signout(): void {
-
+    this._http.get('/api/users', new SignoutRequestOptions())
+    .map(response => response.json())
+    .subscribe(null, null, () => this._unsetSignedIn());
   }
 
   public signup(): void {
