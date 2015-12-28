5b449fd646f14cbfe2c12eab47ed41c64e8e90a0
Mikko Karvonen <mikko.karvonen@ymail.com>
2015-12-28 17:30:43 +0200

    Add Bootstrap navbar.

---
+++ src/public/app/shared/directives/navbar.ts
@@ -0,0 +1,37 @@
+import {NgIf} from 'angular2/common';
+import {Component} from 'angular2/core';
+import {RouterLink} from 'angular2/router';
+import {User, UserService} from '../services/userService';
+
+@Component({
+  directives: [NgIf, RouterLink],
+  pipes: [],
+  providers: [],
+  selector: 'navbar',
+  template: require('./navbar.html')
+})
+export class Navbar {
+
+  public userName: string = '';
+  public isSignedIn: boolean = false;
+
+  constructor(private _userService: UserService) {
+    console.debug('Navbar constructor.');
+    this._subscribeUserObserver();
+  }
+
+  private _subscribeUserObserver(): void {
+    this._userService.user$.subscribe(
+      (user: User) => {
+        if (user !== null) {
+          this.userName = user.name.formatted;
+          this.isSignedIn = true;
+        } else {
+          this.userName = '';
+          this.isSignedIn = false;
+        }
+      }
+    );
+  }
+
+}
