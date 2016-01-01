dd0561a3dceff2f02ead96f33d2ac0bb507292b1
Mikko Karvonen <mikko.karvonen@ymail.com>
2016-01-01 21:05:11 +0200

    Chat component get SocketServices in ngOnInit, clean them up on ngOnDestroy.

--- src/public/app/components/chat/chat.ts
+++ src/public/app/components/chat/chat.ts
@@ -1,6 +1,7 @@
 import {Component} from 'angular2/core';
 import {NgModel, NgFor} from 'angular2/common';
 import {Router} from 'angular2/router';
+import {Subscription} from 'rxjs/Subscription';
 import {SocketService} from '../../shared/services/socketService';
 
 let chatInstance;
@@ -16,22 +17,34 @@ export class Chat {
 
   private _input: string;
   private _messages: Array<string> = [];
+  private _socketServiceSubsc: Subscription<boolean>;
 
   constructor(private _router: Router, private _socketService: SocketService) {
     console.debug('Chat constructor.');
-    this._socketService.connection$.subscribe(
-      (connected: boolean) => {
-        if (connected) {
-          this._socketService.registerReceiver('chat', this.onReceive);
-          this._socketService.emit('chat', {event: 'join'});
-        }
-      }
-    );
     chatInstance = this;
   }
 
   public getMessages(): Array<string> {
     return this._messages;
+  }
+
+  public ngOnDestroy() {
+    console.debug('Chat ngOnDestroy');
+    this._socketService.emit('chat', {event: 'left'});
+    this._socketService.deregisterReceiver('chat');
+    this._socketServiceSubsc.unsubscribe();
+  }
+
+  public ngOnInit() {
+    console.debug('Chat ngOnInit');
+    this._socketService.registerReceiver('chat', this.onReceive);
+    this._socketServiceSubsc = this._socketService.connection$.subscribe(
+      (connected: boolean) => {
+        if (connected) {
+          this._socketService.emit('chat', {event: 'joined'});
+        }
+      }
+    );
   }
 
   public onReceive(data: string): void {
