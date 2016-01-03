608080095412716da4674f17126f704388e521b9
Mikko Karvonen <mikko.karvonen@ymail.com>
2016-01-03 16:30:11 +0200

    Add webpack.config.

---
+++ webpack.config.js
@@ -0,0 +1,69 @@
+var path = require('path');
+var webpack = require('webpack');
+var CommonsChunkPlugin = webpack.optimize.CommonsChunkPlugin;
+var CopyWebpackPlugin = require('copy-webpack-plugin');
+
+module.exports = {
+  entry: {
+    app: './src/public/bootstrap.ts',
+    vendor: './src/public/vendor.ts'
+  },
+
+  output: {
+    chunkFilename: '[id].chunk.js',
+    path: './build/public/app',
+    filename: '[name].js',
+    publicPath: '/app/'
+  },
+
+  plugins: [
+    new webpack.ProvidePlugin({
+      $: 'jquery', jQuery: 'jquery', 'window.jQuery': 'jquery'
+    }),
+    new webpack.optimize.DedupePlugin(),
+    new CommonsChunkPlugin({name: 'vendor', filename: 'vendor.js', minChunks: Infinity}),
+    new CommonsChunkPlugin({name: 'common', filename: 'common.js', minChunks: 2, chunks: ['app', 'vendor']}),
+    new CopyWebpackPlugin([{from: 'src/public/app/index.html', to: '/'}])
+  ],
+
+  bail: true,
+
+  watch: true,
+
+  module: {
+    loaders: [
+      {test: /\.jsx$/, loader: 'jsx?insertPragma=React.DOM&harmony'},
+      {test: /\.css$/, loader: 'style!css'},
+      {test: /\.scss$/,loader: 'style!css!sass'},
+      {test: /\.(gif|jpe|jpg|png|woff|woff2|eot|ttf|svg)(\?.*$|$)/, loader: 'file'},
+      {test: /\.json$/, loader: 'json'},
+      {test: /\.html$/, loader: 'raw'},
+      {test: /\.html\.tmpl$/, loader: 'raw'},
+      {test: /\.node$/, loader: 'node'},
+      {
+        test: /\.ts$/, loader: 'ts',
+        query: {
+          'ignoreDiagnostics': [
+            2403, // 2403 Subsequent variable declarations
+            2300, // 2300 Duplicate identifier
+            2375, // 2375 Duplicate string index signature
+            2420  // 2420 Class 'EventEmitter' incorrectly implements interface 'NodeJS.EventEmitter'
+          ]
+        },
+        exclude: [/\.spec\.ts$/, /\.e2e\.ts$/, /node_modules/]
+      },
+    ],
+    noParse: [/zone\.js\/dist\/.+/, /angular2\/bundles\/.+/]
+  },
+
+  resolve: {
+    root: [
+      path.resolve(__dirname, '../src/public/app'),
+      path.resolve(__dirname, '../node_modules'),
+      path.resolve(__dirname, '../src/server')
+    ],
+    extensions: ['', '.ts', '.js', '.json', '.css', '.html', '.html.tmpl', '.node']
+  },
+
+  node: {fs: 'empty', net: 'empty', tls: 'empty'},
+};
