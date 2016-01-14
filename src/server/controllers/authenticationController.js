d6e61407246cc1350d028c7aef75e45c9e9699eb
Mikko Karvonen <onttoni@gmail.com>
2016-01-14 14:15:51 +0200

    Use token authentication on user api when possible.

--- src/server/controllers/authenticationController.js
+++ src/server/controllers/authenticationController.js
@@ -1,10 +1,8 @@
 var expressJwt = require('express-jwt');
 var jwtPublic = require('../token').getPublicKey();
 
-module.exports.controller = function(app, apiPrefix) {
-
-  app.all(apiPrefix + 'friends/',
-  expressJwt({
+function expressJwtFactory() {
+  return expressJwt({
     secret: jwtPublic
   }),
   function(err, req, res, next) {
@@ -12,5 +10,15 @@ module.exports.controller = function(app, apiPrefix) {
       return res.status(401).send({msg: 'unauthorized'});
     }
     next();
-  });
+  };
+}
+
+module.exports.controller = function(app, apiPrefix) {
+
+  app.all(apiPrefix + 'friends/', expressJwtFactory());
+
+  app.get(apiPrefix + 'users', expressJwtFactory());
+
+  app.put(apiPrefix + 'users', expressJwtFactory());
+
 };
