d9018c675fe5a816e27a99475de71e590dd48256
Mikko Karvonen <onttoni@gmail.com>
2016-01-19 11:00:25 +0200

    Extend Modal with UserSignin.

--- src/public/app/components/user/signin.ts
+++ src/public/app/components/user/signin.ts
@@ -1,6 +1,7 @@
 import {Component} from 'angular2/core';
 import {Router, RouterLink} from 'angular2/router';
 import {User, UserService} from '../../shared/services/userService';
+import {Modal} from '../../shared/services/modalService';
 
 @Component({
   directives: [RouterLink],
@@ -9,22 +10,24 @@ import {User, UserService} from '../../shared/services/userService';
   selector: 'signin',
   template: require('./signin.html')
 })
-export class UserSignin {
+export class UserSignin extends Modal {
   public email: string;
   public password: string;
 
   constructor(private _router: Router, private _userService: UserService) {
+    super();
     console.debug('UserSignin constructor.');
   }
 
   public onCancel(): void {
-    this._router.navigate(['Home']);
+    this._whenClosedObserver.next(true);
   }
 
   public onSubmit(): void {
     this._userService.user$.subscribe(
       (user: User) => {
         if (user !== null) {
+          this._whenClosedObserver.next(true);
           this._router.navigate(['Home']);
         }
       }
