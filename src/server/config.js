5b4274040023f1c5ba0da6dde3b69c0fa86b090d
Mikko Karvonen <onttoni@gmail.com>
2016-01-19 15:44:33 +0200

    Add session key and Google OAuth configs.

--- src/server/config.js
+++ src/server/config.js
@@ -9,13 +9,17 @@ cfg.express.key = path.join(__dirname, 'keys/ssl_key.pem');
 cfg.express.cert = path.join(__dirname, 'keys/ssl_cert.pem');
 
 cfg.expressSession = {};
-cfg.expressSession.secret = 'foobar';
+cfg.expressSession.secret = path.join(__dirname, 'keys/session-secret.key');;
 
 cfg.jsonwebtoken = {};
 cfg.jsonwebtoken.private = path.join(__dirname, 'keys/jwt_private_key.pem');
 cfg.jsonwebtoken.public = path.join(__dirname, 'keys/jwt_public_key.pem');
 cfg.jsonwebtoken.expiresIn = '24h';
 cfg.jsonwebtoken.sessionFallback = false;
+
+cfg.google = {};
+cfg.google.id = path.join(__dirname, 'keys/google-client-id.key');
+cfg.google.secret = path.join(__dirname, 'keys/google-client-secret.key');
 
 cfg.mongo = {};
 cfg.mongo.host = 'mongo01.local';
