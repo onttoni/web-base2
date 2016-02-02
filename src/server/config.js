092e0cf11e09ee627c167420eea9836ce37e2fc0
Mikko Karvonen <onttoni@gmail.com>
2016-02-02 12:52:49 +0200

    Add googleOAuth2 config.

--- src/server/config.js
+++ src/server/config.js
@@ -4,6 +4,7 @@ cfg = {};
 module.exports = cfg;
 
 cfg.express = {};
+cfg.express.hostName = 'localhost';
 cfg.express.httpPort = 8080;
 cfg.express.key = path.join(__dirname, 'keys/ssl_key.pem');
 cfg.express.cert = path.join(__dirname, 'keys/ssl_cert.pem');
@@ -17,9 +18,14 @@ cfg.jsonwebtoken.public = path.join(__dirname, 'keys/jwt_public_key.pem');
 cfg.jsonwebtoken.expiresIn = '24h';
 cfg.jsonwebtoken.sessionFallback = false;
 
-cfg.google = {};
-cfg.google.id = path.join(__dirname, 'keys/google-client-id.key');
-cfg.google.secret = path.join(__dirname, 'keys/google-client-secret.key');
+cfg.googleOAuth2 = {};
+cfg.googleOAuth2.enabled = true;
+cfg.googleOAuth2.clientId = path.join(__dirname, 'keys/google-client-id.key');
+cfg.googleOAuth2.clientSecret = path.join(__dirname, 'keys/google-client-secret.key');
+cfg.googleOAuth2.callback = '/auth/callback-google';
+cfg.googleOAuth2.redirectURL =
+  'https://' +  cfg.express.hostName + ':' + cfg.express.httpPort + cfg.googleOAuth2.callback;
+cfg.googleOAuth2.scopes = ['email', 'profile'];
 
 cfg.mongo = {};
 cfg.mongo.host = 'mongo01.local';
