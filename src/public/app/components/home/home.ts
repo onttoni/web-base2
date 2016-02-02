f1dbfa9dc9e7499a7961b83e3c3884be8cba3a00
Mikko Karvonen <onttoni@gmail.com>
2016-02-02 13:14:19 +0200

    Capture local access-code in Home constructor for now.

--- src/public/app/components/home/home.ts
+++ src/public/app/components/home/home.ts
@@ -1,6 +1,8 @@
+import _ = require('lodash');
 import {Component} from 'angular2/core';
-import {RouterLink} from 'angular2/router';
+import {RouteParams, Router, RouterLink} from 'angular2/router';
 import {ModalService} from '../../shared/services/modalService';
+import {UserService} from '../../shared/services/userService';
 
 @Component({
   directives: [RouterLink],
@@ -10,8 +12,14 @@ import {ModalService} from '../../shared/services/modalService';
   template: require('./home.html')
 })
 export class Home {
-  constructor(private _modalService: ModalService) {
+  constructor(private _params: RouteParams, private _router: Router, private _userService: UserService) {
     console.debug('Home constructor.');
+    // TODO: Global interceptor for access-code
+    let accessCode: any = _.get(this, '_params.params.access-code');
+    if (accessCode) {
+      this._userService.signinAccessCode(accessCode);
+      this._router.navigate(['Home']);
+    }
   }
 
   public reload() {
