93f5bb14a49edb73ce796a779c9259e8bac620d4
Mikko Karvonen <onttoni@gmail.com>
2016-02-02 13:13:27 +0200

    Add method goTo in WindowService.

--- src/public/app/shared/services/windowService.ts
+++ src/public/app/shared/services/windowService.ts
@@ -7,7 +7,12 @@ export class WindowService {
     console.debug('WindowService constructor');
   }
 
-  public openPopup(url: string, width = 480, height = 640) {
+  public goTo(url: string): void {
+    console.debug(`WindowService goTo ${url}`);
+    window.location.href = url;
+  }
+
+  public openPopup(url: string, width = 480, height = 640): void {
     console.debug(`WindowService openPopup ${url}`);
     let left = (screen.availWidth / 2) - (width / 2);
     let top = (screen.availHeight / 2) - (height / 2);
